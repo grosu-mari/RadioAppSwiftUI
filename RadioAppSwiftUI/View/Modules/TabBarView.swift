ca71a0d356e85478dccf05a164bf2cfd88b548ef
Nikita Kazakov <nskazakov>
2021-10-23 18:16:27 +0300

    Create TabBarView.

---
+++ RadioAppSwiftUI/View/Modules/TabBarView.swift
@@ -0,0 +1,42 @@
+//
+//  TabBarView.swift
+//  RadioAppSwiftUI
+//
+//  Created by Маша Боянжу on 23.10.2021.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom), content: {
+            TabView {
+                RadioView()
+                    .padding(.top, 1)
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Медиатека")
+                    }
+                Text("Радио")
+                    .tabItem {
+                        Image(systemName: "dot.radiowaves.left.and.right")
+                        Text("Радио")
+                    }
+                Text("Поиск")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Поиск")
+                    }
+            }
+            .accentColor(.red)
+            MiniPlayer()
+        })
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
+
