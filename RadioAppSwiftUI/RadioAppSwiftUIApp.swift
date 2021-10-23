368fd7cee4258b0f637ecf7a95c0dbbc02847add
Nikita Kazakov <nskazakov>
2021-10-23 18:05:39 +0300

    Initial Commit

---
+++ RadioAppSwiftUI/RadioAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioAppSwiftUIApp.swift
+//  RadioAppSwiftUI
+//
+//  Created by Маша Боянжу on 23.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct RadioAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
